2744f8e8c640c1897600bdc3cf78b5baad40cf64
sprd39 <76632782+sprd39@users.noreply.github.com>
2023-04-14 13:33:55 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,45 +1,50 @@
-canvas = document.getElementById("myCanvas");
-ctx = canvas.getContext("2d");
-var mouseEvent = "empty";
-var last_positions_of_x, last_positions_of_y;
-var color = "green";
-var width_of_line = 1;
-canvas.addEventListener("mousedown", my_mousedown);
-
-function my_mousedown(e)
+var names_of_people = [];
+    
+function submit()
 {
-    mouseEvent = "mouseDown"
+    var GuestName = document.getElementById("name1").value;
+	// use the push function as - names_of_people.push(GuestName);
+	names_of_people.push(GuestName);
+	console.log(GuestName);    
+    console.log(names_of_people);
+    var lenght_of_name = names_of_people.length;
+    console.log(lenght_of_name);
+	document.getElementById("display_name").innerHTML=names_of_people.toString();
+   }
+
+
+
+function show()
+{
+	var i= names_of_people.join("<br>");
+	console.log(names_of_people);
+	document.getElementById("p1").innerHTML=i.toString();
+	document.getElementById("sort_button").style.display="block";
+	
 }
-canvas.addEventListener("mouseleave", my_mouseleave);
-function my_mouseleave(e)
-{
-    mouseEvent = "mouseleave"
-}
-canvas.addEventListener("mouseup", my_mouseup);
-function my_mouseup(e)
-{
-    mouseEvent = "mouseUP"
-}
-canvas.addEventListener("mousemove", my_mousemove);
-function my_mousemove(e)
-{
-    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
-    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
 
-    if (mouseEvent == "mouseDown") {
-    ctx.beginPath();
-    ctx.strokeStyle = color;
-    ctx.lineWidth = width_of_line;
 
-    console.log("Last position of x and y coordinates = ");
-    console.log("x = " + last_positions_of_x + "y = " + last_positions_of_y);
-    ctx.moveTo(last_positions_of_x, last_positions_of_y);
+function sorting()
+	{
+		names_of_people.sort();           // add the sort function here
+		// .sort();
+		var i= names_of_people.join("<br>");
+		console.log(names_of_people);		
+		document.getElementById("sorted").innerHTML=i.toString();
+		}
 
-    console.log("Currant position of x and y coordinates = ")
-    console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
-    ctx.lineTo(current_position_of_mouse_x, current_position_of_mouse_y);
-    ctx.stroke();
-    }
-    last_positions_of_x = current_position_of_mouse_x;
-    last_positions_of_y = current_position_of_mouse_y;
+
+function searching()
+{
+	var s= document.getElementById("s1").value;
+	var found=0;
+	var j;
+	for(j=0; j<names_of_people.length; j++)
+		{
+			if(s==names_of_people[j]){
+				found=found+1;
+			}	
+		}
+	document.getElementById("p2").innerHTML="name found "+found+" time/s";
+	console.log("found name "+found+" time/s");
 }
